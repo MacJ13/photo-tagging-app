fbd187982314d143fbc91bf4ea61af1fa97814ac
MacJ13 <jacdim02@gmail.com>
2025-10-07 22:11:59 +0200

    init express node app
    - init new npm project
    - install required dependencies (express and typescript with proper
      tools)
    - configure typescript with tsc --init
    - edit tsconfig.json
    - create src root folder
    - create app.ts to run express server
    - add 'dev', 'build' and 'start' scripts in package.json
    
    - install Jest, Supertest, their TypeScript types, and ts-jest
      dependencies
    - create jest.config.ts file

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  moduleFileExtensions: ["ts", "js", "json"],
+  transform: {
+    "^.+\\.ts$": "ts-jest",
+  },
+  testMatch: ["**/__tests__/**/*.test.ts"],
+};
+
+export default config;
---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, Typescript with Express");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
