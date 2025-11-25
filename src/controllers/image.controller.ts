5e8e9bf677a2c661f41c3bd4fe6b11f0863ae227
MacJ13 <jacdim02@gmail.com>
2025-11-25 22:42:34 +0100

    show possibly error message in image upload view
    - create image_upload_post controller handler
    - check possible error appearing
    - render imageUpload.ejs view with error message if error appears
    - create POST /upload route in image.route.ts with
      upload.single("image"), multerErrorHandler
        middlewares and 'image_upload_post' handler controller

--- src/controllers/image.controller.ts
+++ src/controllers/image.controller.ts
@@ -9,7 +9,41 @@ const image_upload_get: HandlerType = (req, res, next) => {
   res.render("pages/imageUpload", { title: "Upload Images" });
 };
 
+const image_upload_post: HandlerType = (req, res, next) => {
+  // console.log(req.fileUloadError, "---", Boolean(req.fileUloadError));
+
+  if (req.fileUloadError) {
+    console.log("File upload error:", req.fileUloadError);
+
+    return res.render("pages/imageUpload", {
+      title: "Upload Images",
+      errorMessage: req.fileUloadError,
+    });
+  }
+
+  if (!req.file) {
+    console.log("No file uploaded");
+    return res.render("pages/imageUpload", {
+      title: "Upload Images",
+      errorMessage: "No file uploaded",
+    });
+  }
+
+  try {
+    console.log("file uploaded successfully");
+    console.log(req.file);
+    // THIS WILL CONTINUE TO SAVE THE FILE TO DB OR FILE SYSTEM
+    res.render("pages/imageUpload", { title: "Upload Images" });
+  } catch (error) {
+    res.status(500).render("pages/imageUpload", {
+      title: "Upload Images",
+      errorMessage: "An error occurred while processing the image",
+    });
+  }
+};
+
 export default {
   image_index_get,
   image_upload_get,
+  image_upload_post,
 };
--- src/routes/image.route.ts
+++ src/routes/image.route.ts
@@ -1,10 +1,23 @@
 import express from "express";
 import imageController from "../controllers/image.controller";
 
+import { multerErrorHandler } from "../middlewares/multerErrorHandler";
+import { requireAuth } from "../middlewares/requireAuth";
+import { upload } from "../config/multer.config";
+
 const imageRouter = express.Router();
+
+imageRouter.use(requireAuth);
 
 imageRouter.get("/", imageController.image_index_get);
 
 imageRouter.get("/upload", imageController.image_upload_get);
+
+imageRouter.post(
+  "/upload",
+  upload.single("image"),
+  multerErrorHandler,
+  imageController.image_upload_post
+);
 
 export default imageRouter;
